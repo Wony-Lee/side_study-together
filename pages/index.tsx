643d8b96316712650eacfc6d504876db959c67cc
Wony-Lee <a1_11_11@naver.com>
2022-04-23 22:10:44 +0900

    import Error update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from "next";
 import AppLayout from "../components/AppLayout";
 import GroupList from "../components/Content/GroupList";
-import Frame from "../components/CreateStudy/Frame";
-import LoginForm from "../components/Login/LoginForm";
-import SignUp from "../components/SignUp/SignUp";
-import TermsOfService from "../components/SignUp/TermsOfService";
 import Modal from "../components/Modal/Modal";
 
 const Home: NextPage = () => {
